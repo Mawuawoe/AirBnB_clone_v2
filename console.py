76d25f92a75943fd2793bd1983a1c52965869de0
Mawuawoe <desmonddzakago@gmail.com>
2024-07-05 16:19:39 +0000

    task_1

--- console.py
+++ console.py
@@ -36,12 +36,12 @@ class HBNBCommand(cmd.Cmd):
         if not sys.__stdin__.isatty():
             print('(hbnb)')
 
-    """def precmd(self, line):
-        Reformat command line for advanced command syntax.
+    def precmd(self, line):
+        """Reformat command line for advanced command syntax.
 
         Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
         (Brackets denote optional fields in usage example.)
-
+        """
         _cmd = _cls = _id = _args = ''  # initialize line elements
 
         # scan for general formating - i.e '.', '(', ')'
@@ -85,7 +85,7 @@ class HBNBCommand(cmd.Cmd):
         except Exception as mess:
             pass
         finally:
-            return line"""
+            return line
 
     def postcmd(self, stop, line):
         """Prints if isatty is false"""
