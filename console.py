efcfe2b4caaeff87159291a7a990e6c642f38448
Mawuawoe <desmonddzakago@gmail.com>
2024-07-05 20:28:11 +0000

    task_1

--- console.py
+++ console.py
@@ -132,7 +132,7 @@ class HBNBCommand(cmd.Cmd):
                 key, value = pair.split('=', 1)
                 # Handle string values
                 if value.startswith('"') and value.endswith('"'):
-                    value = value[1:-1]
+                    value = value[1:-1].replace('_', ' ')
                 else:
                     # Convert to the appropriate type
                     try:
