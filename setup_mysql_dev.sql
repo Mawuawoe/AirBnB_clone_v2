b7bb17ad95b4c2dc1c64d5b3dba838a8f1d39d6f
Mawuawoe <desmonddzakago@gmail.com>
2024-07-08 13:07:45 +0000

    task_4

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create user
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'Driftpiloloo@13';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges on the hbnb_dev_db database to the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
