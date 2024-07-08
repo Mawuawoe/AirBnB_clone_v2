29480bfdec88e92eed3edf3706a04d9aa59f18ee
Mawuawoe <desmonddzakago@gmail.com>
2024-07-08 14:25:30 +0000

    task-8

--- models/city.py
+++ models/city.py
@@ -6,16 +6,25 @@ that create the city table
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
+import os
 
 
 class City(BaseModel, Base):
     """
     The city class, contains state ID and name
     """
-    __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        name = Column(String(128), nullable=False)
 
-    places = relationship("Place",
+        places = relationship("Place",
                           backref="cities",
                             cascade="all, delete-orphan")
+    else:
+        name = ""
+        state_id = ""
+    
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
--- models/user.py
+++ models/user.py
@@ -8,12 +8,22 @@ import os
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
 
-    places = relationship("Place",
-                          backref="user",
-                            cascade="all, delete-orphan")
+        places = relationship("Place",
+                                backref="user",
+                                cascade="all, delete-orphan")
+    else:
+        email = ""
+        _password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
