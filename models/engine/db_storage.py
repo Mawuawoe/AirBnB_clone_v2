67b10031e45c1e54c1ea9685fb99f4750f312728
Mawuawoe <desmonddzakago@gmail.com>
2024-07-08 12:55:02 +0000

    task-7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from models.base_model import Base
 from models.state import State
 from models.city import City
+from models.user import User
 from urllib.parse import quote_plus
 
 
@@ -52,7 +53,7 @@ class DBStorage:
                     key = f"{obj.__class__.__name__}.{obj.id}"
                     obj_dict[key] = obj
         else:
-            classes = [State, City]
+            classes = [State, City, User]
             for class_type in classes:
                 q_result = self.__session.query(class_type).all()
                 for obj in q_result:
