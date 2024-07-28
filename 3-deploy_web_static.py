f573451074ec00ee09a789314eaea23d714916e8
Mawuawoe <desmonddzakago@gmail.com>
2024-07-28 02:15:07 +0000

    task-3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,35 +1,48 @@
 #!/usr/bin/python3
 """
-A sript to pack & deploy
+A script that packs and deploy web_static to server
 """
+
 from datetime import datetime
-from fabric.api import env, local, put, run, task
-from os.path import exists
+from fabric.api import env, local, run, put, hide
+import os
 
-# Define the remote servers
-env.hosts = ['52.55.249.213', '54.157.32.137']
 
-@task
 def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
-
+    The archive will be named with the
+    urrent timestamp to ensure unique filenames.
     Returns:
-        The path to the created archive, or None if there was an error.
+        None if there is an error during the archiving process.
     """
     try:
+        # Get the current date and time in the format YYYYMMDDHHMMSS
         date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_path = "versions/web_static_{}.tgz".format(date_time)
+        # Suppress dispaly of the command executing
+        with hide('running'):
+            # Print the action being performed
+            local("echo 'Packing web_static to versions/web_static_{}.tgz'"
+                  .format(date_time))
+            # Ensure the versions directory exists
+            local("mkdir -p versions/")
+        # Create a .tgz archive of the web_static directory
+        local("tar -cvzf versions/web_static_{}.tgz web_static"
+              .format(date_time))
+        return "versions/web_static_{}.tgz".format(date_time)
 
-        print('Packing web_static to {}'.format(archive_path))
-        local("mkdir -p versions")
-        local("tar -cvzf {} web_static".format(archive_path))
-        return archive_path
     except Exception as e:
-        print(e)
+        # Return None if there is an error during the process
         return None
 
-@task
+
+# Define the remote servers
+env.hosts = ['54.160.125.157', '52.207.208.124']
+
+# Define the Fabric user and key filename via command-line options (-u and -i)
+# Note: These are placeholders and will be replaced by the command-line options
+
+
 def do_deploy(archive_path):
     """
     Deploys the .tgz archive to the remote servers.
@@ -40,48 +53,50 @@ def do_deploy(archive_path):
     Returns:
         True if successful, False otherwise.
     """
-    if not exists(archive_path):
-        print("Archive path doesn't exist")
+
+    if not os.path.exists(archive_path):
         return False
 
     try:
-        # Extract file name and directory name from the archive path
+        # uplaod zip file to /tmp/
+        put(archive_path, '/tmp/')
+
         file_name = archive_path.split("/")[-1]
         no_ext = file_name.split(".")[0]
-        remote_tmp_path = "/tmp/{}".format(file_name)
         remote_release_path = "/data/web_static/releases/{}".format(no_ext)
 
-        # Upload the archive to the /tmp/ directory on the remote server
-        put(archive_path, remote_tmp_path)
-
         # Create the release directory
-        run("mkdir -p {}".format(remote_release_path))
+        run("sudo mkdir -p {}".format(remote_release_path))
 
         # Uncompress the archive to the release directory
-        run("tar -xzf {} -C {}".format(remote_tmp_path, remote_release_path))
+        run("sudo tar -xzf /tmp/{} -C {}"
+            .format(file_name, remote_release_path))
 
         # Remove the archive from the /tmp/ directory
-        run("rm {}".format(remote_tmp_path))
+        run("sudo rm /tmp/{}".format(file_name))
 
-        # Move the contents of the web_static directory to the release directory
-        run("mv {}/web_static/* {}".format(remote_release_path, remote_release_path))
+        # Move the contents of the web_static directory
+        # to the release directory
+        # extracting create a subdirectory
+        run("sudo mv {}/web_static/* {}"
+            .format(remote_release_path, remote_release_path))
 
         # Remove the now empty web_static directory
-        run("rm -rf {}/web_static".format(remote_release_path))
+        run("sudo rm -rf {}/web_static".format(remote_release_path))
 
         # Delete the current symlink
-        run("rm -rf /data/web_static/current")
+        run("sudo rm -rf /data/web_static/current")
 
         # Create a new symlink to the release directory
-        run("ln -s {} /data/web_static/current".format(remote_release_path))
+        run("sudo ln -s {} /data/web_static/current"
+            .format(remote_release_path))
 
         print("New version deployed!")
         return True
     except Exception as e:
-        print("Deployment failed: {}".format(e))
         return False
 
-@task
+
 def deploy():
     """
     Packs and deploys the static web content to the remote servers.
@@ -91,6 +106,5 @@ def deploy():
     """
     archive_path = do_pack()
     if archive_path is None:
-        print("Packing failed")
         return False
     return do_deploy(archive_path)
