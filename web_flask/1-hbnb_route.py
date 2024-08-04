a1264f688ba0c5ba496cd7ab5cf0f7b5c2c4e003
Mawuawoe <desmonddzakago@gmail.com>
2024-08-04 11:16:27 +0000

    task_1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module starts a simple Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Displays a simple message 'Hello HBNB!' on the root route.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays a simple message 'HBNB!' for /hbnb
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    """
+    Runs the Flask web application on host 0.0.0.0 and port 5000.
+    """
+    app.run(host="0.0.0.0", port=5000)
